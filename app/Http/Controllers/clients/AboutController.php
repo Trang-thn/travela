895634624ac08bb3609e8ca0ecc6e0c07e4dda78
Trang-thn <trangthn234789@gmail.com>
2025-12-24 22:34:09 +0700

    doi1

--- app/Http/Controllers/clients/AboutController.php
+++ app/Http/Controllers/clients/AboutController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers\clients;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class AboutController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return view('clients.home');
+        return view('clients.about');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\clients\HomeController;
+use App\Http\Controllers\clients\AboutController;
 
 // Route::get('/', function () {
 //     return view('home');
 // });
- Route::get('/', [HomeController::class,'index'])->name('home');
+Route::get('/', [HomeController::class,'index'])->name('home');
+Route::get('/about', [AboutController::class,'index'])->name('about');
